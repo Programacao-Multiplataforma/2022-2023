9df6373f6b3e47a806643a001a30efca85899a77
lfbutad <lfb@utad.pt>
2023-05-05 22:57:27 +0100

    Exercício 5 - extra-aula

---
+++ Exercicio5/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+
+ViewModel _viewModel = new ViewModel();
+
+_viewModel.CarregaFicheiro("FicheiroTexto.txt");
+
+Console.WriteLine("Número de Parágrafos : " + _viewModel.NumeroParagrafos);
+Console.WriteLine("Número de Palavras : " + _viewModel.NumeroPalavras);
+Console.WriteLine("Número de Carateres : " + _viewModel.NumeroCaracteres);
+Console.WriteLine("Número de ocorrências da palavra 'alunos' : " + _viewModel.NumeroOcorrencias("alunos"));
---
+++ Exercicio5/ConsoleApp/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ViewModel
+    {
+        // atributos (propriedades) que caracterizam o Model
+        public string Texto { get; private set; }
+
+        
+        public void CarregaFicheiro(string fich)
+        {
+            Texto = File.ReadAllText(fich); // método simplificado de ler o ficheiro
+        }
+
+        // mensagens (métodos) da questão 1
+        public int NumeroParagrafos { 
+            get { 
+                return Texto.Split('\n').Length; 
+            } 
+        }
+
+        public int NumeroPalavras
+        {
+            get
+            {
+                char[] separadores = { '\n', ' ', ',', '.', '!', '?', '\t', '(', ')' }; // outros separadores podem ser adicionados
+                return Texto.Split(separadores, StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+        }
+
+        public int NumeroCaracteres // incluindo caracteres brancos
+        {
+            get
+            {
+                return Texto.Length;
+            }
+        }
+
+        public int NumeroOcorrencias(string palavra)
+        {
+            char[] separadores = { '\n', ' ', ',', '.', '!', '?', '\t', '(', ')' }; // outros separadores podem ser adicionados
+            string[] palavras = Texto.Split(separadores, StringSplitOptions.RemoveEmptyEntries);
+            
+            return palavras.Count(x => x.Equals(palavra));
+        }
+
+    }
+}
